6b63c5e459f51f44909f73b1a83952d4cbe1cafd
Tristan D Gant <tgant2017@outlook.com>
2024-01-15 18:21:52 -0600

    more logging

--- modules/Jimper/lib/InstagramClient.js
+++ modules/Jimper/lib/InstagramClient.js
@@ -25,47 +25,97 @@ const instagramPost = async (client, comicPath) => {
   });
 }
 
+// /**
+//  * Creates a new cookie store, connects to instagram, and then creates the post
+//  * @param {String} comicPath 
+//  */
+// InstagramClient.prototype.makeInstaPost = async (comicPath) => {
+//   const cookieStore = new FileCookieStore('./cookies.json');
+//   const client = new Instagram({
+//     username: INSTAGRAM_USER, 
+//     password: INSTAGRAM_PASSWORD,
+//     cookieStore
+//   },
+//   {
+//     language: 'en-US'
+//   });
+  
+//   console.log('about to log in');
+//   await client.login({ 
+//     username: INSTAGRAM_USER, 
+//     password: INSTAGRAM_PASSWORD 
+//   }, 
+//   {
+//     _sharedData: false
+//   })
+//   .then(async () => {
+//     console.log('LOGIN GOOD');
+//     await instagramPost(client, comicPath);
+
+//   }).catch(async (err) => {
+//     console.log('top of error func');
+//     if(err.error && err.error.message === 'checkpoint_required') {
+//       console.log('checkpoint');
+//       var challengeUrl = err.error.checkpoint_url;
+//       challengeUrl = challengeUrl.replace('https://www.instagram.com', '');
+//       console.log(challengeUrl, typeof challengeUrl);
+//       await client.updateChallenge({ challengeUrl, choice: 1 });
+//       await instagramPost(client, comicPath);
+//     } else {
+//       console.log(err, 'EERROROEROREOERO');
+//     }
+//   });
+// }
+
+
+// rewrite this hoe in a try catch and see if i can get vetter logging
+
 /**
  * Creates a new cookie store, connects to instagram, and then creates the post
  * @param {String} comicPath 
  */
 InstagramClient.prototype.makeInstaPost = async (comicPath) => {
-  const cookieStore = new FileCookieStore('./cookies.json');
-  const client = new Instagram({
-    username: INSTAGRAM_USER, 
-    password: INSTAGRAM_PASSWORD,
-    cookieStore
-  },
-  {
-    language: 'en-US'
-  });
-  
-  console.log('about to log in');
-  await client.login({ 
-    username: INSTAGRAM_USER, 
-    password: INSTAGRAM_PASSWORD 
-  }, 
-  {
-    _sharedData: false
-  })
-  .then(async () => {
-    console.log('LOGIN GOOD');
-    await instagramPost(client, comicPath);
+  try {
+    const cookieStore = new FileCookieStore('./cookies.json');
+    const client = new Instagram({
+      username: INSTAGRAM_USER, 
+      password: INSTAGRAM_PASSWORD,
+      cookieStore
+    },
+    {
+      language: 'en-US'
+    });
 
-  }).catch(async (err) => {
-    console.log('top of error func');
+    console.log('about to log in');
+    await client.login({ 
+      username: INSTAGRAM_USER, 
+      password: INSTAGRAM_PASSWORD 
+    }, 
+    {
+      _sharedData: false
+    });
+
+    console.log('LOGGED IN');
+
+
+  }
+  catch(error) {
+    console.log('ERROR');
     if(err.error && err.error.message === 'checkpoint_required') {
       console.log('checkpoint');
       var challengeUrl = err.error.checkpoint_url;
       challengeUrl = challengeUrl.replace('https://www.instagram.com', '');
-      console.log(challengeUrl, typeof challengeUrl);
       await client.updateChallenge({ challengeUrl, choice: 1 });
+      await client.login({ 
+        username: INSTAGRAM_USER, 
+        password: INSTAGRAM_PASSWORD 
+      }, 
+      {
+        _sharedData: false
+      });
       await instagramPost(client, comicPath);
     } else {
       console.log(err, 'EERROROEROREOERO');
-    }
-  });
+    } 
+  }
 }
-
-
-// rewrite this hoe in a try catch and see if i can get vetter logging
