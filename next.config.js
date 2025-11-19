2a328c1fa85a391bb5ffc4a058c1ae4737b615e5
Tristan D Gant <tgant2017@outlook.com>
2025-11-19 14:06:34 -0600

    Allow images to render

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        serverComponentsExternalPackages: ['jimp', 'tough-cookie-filestore2']
+        serverComponentsExternalPackages: ['jimp', 'tough-cookie-filestore2'],
+        outputFileTracingIncludes: {
+            '/api/generate-comic': [
+                './PANELz/**/*',
+                './Backgrounds/**/*',
+                './fonts/**/*'
+            ]
+        }
     }
 };
 
